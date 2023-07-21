d69b47b669f6ba1cdfef5aaa9a1fcbb2bb629f77
SalFin99 <SalFin.99@gmail.com>
2023-07-21 12:37:01 +0200

    CAPFM cleaned and new CSV created

--- getPolicies/loadCAPMF.py
+++ getPolicies/loadCAPMF.py
@@ -3,12 +3,15 @@ import numpy as np
 import csv
 
 def loadCAPFMdb():
+    #load the whole CAPFM database and rename columns
     df=pd.read_csv("data/CAPMF/CAPMF_Comp_10022023.csv", delimiter=";",decimal=".")
     df = df.rename(columns={"ISO":"Country", "Component":"Policy", "Comp":"Stringency"})
 
     return df
 
 def loadExcludedPolicies():
+
+    #load file with the policies that I won't consider
 
     with open("data/CAPMF/excludedPolicies.csv", "r", encoding='utf-8-sig') as file:
         ExcludedPolicies = [item for sublist in csv.reader(file) for item in sublist]
@@ -18,6 +21,9 @@ def loadExcludedPolicies():
 
 
 def cleanCAPFMpolicies():
+
+    # clean the CAPFM database deleting rows for not relevant policies
+
     df = loadCAPFMdb()
     ExcludedPolicies = loadExcludedPolicies()
 
@@ -25,6 +31,7 @@ def cleanCAPFMpolicies():
 
     df = df[~df.Policy.isin(ExcludedPolicies)]
 
+    #save the new cleaned database to a CSV.
     #df.to_csv("data/CAPMF/cleanedCAPMF.csv", index=False)
 
     return df
