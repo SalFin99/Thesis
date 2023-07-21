c0434400e09bd1eb1c9b76df8c8a697b8a724e9b
SalFin99 <SalFin.99@gmail.com>
2023-07-21 18:46:07 +0200

    extracted OECD and EU data, created CSV files

--- getPolicies/loadCAPMF.py
+++ getPolicies/loadCAPMF.py
@@ -2,32 +2,28 @@ import pandas as pd
 import numpy as np
 import csv
 
-def loadCAPFMdb():
-    #load the whole CAPFM database and rename columns
+def loadCAPFMdb():    #load the whole CAPFM database and rename columns
+
     df=pd.read_csv("data/CAPMF/CAPMF_Comp_10022023.csv", delimiter=";",decimal=".")
     df = df.rename(columns={"ISO":"Country", "Component":"Policy", "Comp":"Stringency"})
 
     return df
 
-def loadExcludedPolicies():
+def loadExcludedPolicies():     #load file with the policies that I won't consider
 
-    #load file with the policies that I won't consider
+
 
     with open("data/CAPMF/excludedPolicies.csv", "r", encoding='utf-8-sig') as file:
         ExcludedPolicies = [item for sublist in csv.reader(file) for item in sublist]
 
     return ExcludedPolicies
 
+def cleanCAPFMpolicies():    # clean the CAPFM database deleting rows of not relevant policies
 
 
-def cleanCAPFMpolicies():
-
-    # clean the CAPFM database deleting rows for not relevant policies
 
     df = loadCAPFMdb()
     ExcludedPolicies = loadExcludedPolicies()
-
-    print(ExcludedPolicies)
 
     df = df[~df.Policy.isin(ExcludedPolicies)]
 
@@ -36,7 +32,37 @@ def cleanCAPFMpolicies():
 
     return df
 
+def getEU():
+    df = cleanCAPFMpolicies()
 
+    df = df.loc[df['Country'] == 'EUR']
+
+    #df.to_csv("data/CAPMF/EU_cleanedCAPMF.csv", index=False)
+
+    return df
+
+
+def getEU26():
+    EU26 = ['AUT', 'BEL', 'BGR', 'CZE', 'DEU', 'DNK', 'ESP', 'EST', 'FIN', 'FRA', 'GRC', 'HUN', 'HRV', 'IRL', 'ITA', 'LTU', 'LUX', 'LVA', 'MLT', 'NLD', 'POL', 'PRT', 'ROU', 'SVN','SVK', 'SWE']
+
+    df = cleanCAPFMpolicies()
+
+    df = df[df.Country.isin(EU26)]
+
+    #df.to_csv("data/CAPMF/EU26_cleanedCAPMF.csv", index=False)
+
+    return df
+
+def getNonEU():
+    EU26 = ['AUT', 'BEL', 'BGR', 'CZE', 'DEU', 'DNK', 'ESP', 'EST', 'FIN', 'FRA', 'GRC', 'HUN', 'HRV', 'IRL', 'ITA', 'LTU', 'LUX', 'LVA', 'MLT', 'NLD', 'POL', 'PRT', 'ROU', 'SVN','SVK', 'SWE']
+
+    df = cleanCAPFMpolicies()
+
+    df = df[~df.Country.isin(EU26)]
+
+    df.to_csv("data/CAPMF/NonEU_cleanedCAPMF.csv", index=False)
+
+    return df
 
 
 
