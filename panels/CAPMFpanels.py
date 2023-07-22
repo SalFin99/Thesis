7c9b6bc18f5a03f32211040f8f51826c6b87fbe1
SalFin99 <SalFin.99@gmail.com>
2023-07-22 14:14:32 +0200

    Modified df.pivot to df.pivot_table and added function to build the control group

--- panels/CAPMFpanels.py
+++ panels/CAPMFpanels.py
@@ -2,12 +2,25 @@ from getPolicies import loadCAPMF, loadEPS
 import numpy as np
 import pandas as pd
 
+"""
+These functions create the pivot tables for climate policy stringency data. 
+Every table is indexed by country and year.
+The columns represent the different policies.
+The pivot table syntax says that for every ‘index’, return the ‘aggfunc’ of the ‘values’ column(s), 
+segregated (further grouped) by ‘columns’.
+
+Tables are built for EU (aggregated EU level), EU26 countries, Non-EU countries, sample of Non-EU countries. 
+The sample must be specified in the loadCAPMF.getControl() function, in the nonEU[] list. 
+This sample will be then used to test the common trend assumption of DID and select countries for the control group.
+"""
+
+
 def buildPanel_EU():
     df = loadCAPMF.getEU()
 
     df.year=pd.to_datetime(df['year'], format='%Y').dt.year
 
-    panel_EU = df.pivot(index=['Country', 'year'], columns='Policy', values='Stringency')
+    panel_EU = df.pivot_table(index=['Country', 'year'], columns='Policy', values='Stringency')
 
     #df.to_csv("data/CAPMF/panels/EU_panel.csv", index=False)
 
@@ -19,9 +32,10 @@ def buildPanel_EU26():
 
     df.year=pd.to_datetime(df['year'], format='%Y').dt.year
 
-    panel_EU26 = df.pivot(index=['Country', 'year'], columns='Policy', values='Stringency')
+    panel_EU26 = df.pivot_table(index=['Country', 'year'], columns='Policy', values='Stringency', aggfunc=np.mean)
 
-    df.to_csv("data/CAPMF/panels/EU26_panel.csv", index=False)
+
+    #df.to_csv("data/CAPMF/panels/EU26_panel.csv", index=False)
 
     return panel_EU26
 
@@ -30,9 +44,18 @@ def buildPanel_NonEU():
 
     df.year=pd.to_datetime(df['year'], format='%Y').dt.year
 
-    panel_NonEU26 = df.pivot(index=['Country', 'year'], columns='Policy', values='Stringency')
+    panel_NonEU26 = df.pivot_table(index=['Country', 'year'], columns='Policy', values='Stringency')
 
-    df.to_csv("data/CAPMF/panels/NonEU26_panel.csv", index=False)
+    #df.to_csv("data/CAPMF/panels/NonEU26_panel.csv", index=False)
+
+    return panel_NonEU26
+
+def buildPanel_Control():
+    df = loadCAPMF.getControl()
+
+    panel_NonEU26 = df.pivot_table(index=['Country', 'year'], columns='Policy', values='Stringency')
+
+    #df.to_csv("data/CAPMF/panels/NonEU26_panel.csv", index=False)
 
     return panel_NonEU26
 
