301b4acd2042d1fc2a4fb1a3d2a0bf5eed2c7070
SalFin99 <SalFin.99@gmail.com>
2023-08-01 16:01:14 +0200

    added function to load import ratio

--- ImportData/getImport.py
+++ ImportData/getImport.py
@@ -19,7 +19,7 @@ import pandas as pd
         df = pd.read_csv('data/imports/Brazil/FILE.csv', delimiter=';', thousands=".", decimal=',')
 
 """
-def loadImports(dataImportCountry_HScsv):
+def loadSingleImports(dataImportCountry_HScsv):
     df = pd.read_csv(dataImportCountry_HScsv, delimiter=';', thousands=".", decimal=',')
     df.Year=pd.to_datetime(df['Year'], format='%Y').dt.year
     df.set_index('Year', inplace=True)
@@ -27,16 +27,35 @@ def loadImports(dataImportCountry_HScsv):
 
     return df
 
-
-"""
-def loadQty():
-    df=pd.read_excel("data/imports/test1990to2021.xlsx", decimal=".")
-
-    df.Year=pd.to_datetime(df['Year'], format='%Y').dt.year
-
-    df=df.pivot_table(values="Qty", index="Year", columns=["Country"])
-
-    print(df)
+def loadImportRatio(RatioCountrycsv):
+    df = pd.read_csv(RatioCountrycsv, delimiter=';', decimal=',')
+    df.Year = pd.to_datetime(df['Year'], format='%Y').dt.year
+    df.set_index('Year', inplace=True)
+    df = df.pivot_table(index=["Year"], values=["import_ratio"])  # values=["Value", "Weight"]
 
     return df
-""" #load quantity
+
+def loadAllImports():
+    df = pd.read_csv('data/imports/EU/EU_UN.csv', delimiter=';', thousands=".", decimal=',')
+    df.Year=pd.to_datetime(df['Year'], format='%Y').dt.year
+    df.set_index('Year', inplace=True)
+    df=df.pivot_table(index=["HScode","Year"], values=["Value"]) #values=["Value", "Weight"]
+
+    grouped = df.groupby('HScode') #this is a tuple
+
+    df_dictionary = {}
+
+    for hs, group in grouped: #iterate through the tuple. hs gets the key, group gets the values
+        df_dictionary[hs] = group.copy()
+
+    df850231 = df_dictionary[850231]
+    df850231.to_csv('data/imports/EU/EU_850231UN.csv')
+
+    df280530 = df_dictionary[280530]
+    df280530.to_csv('data/imports/EU/EU_280530UN.csv')
+
+    df850511 = df_dictionary[850511]
+    df850511.to_csv('data/imports/EU/EU_850511UN.csv')
+
+    df854140 = df_dictionary[854140]
+    df854140.to_csv('data/imports/EU/EU_854140UN.csv')
